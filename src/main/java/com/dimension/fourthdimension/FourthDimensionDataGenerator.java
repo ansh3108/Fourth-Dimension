b729aa0c03e2260245745d795bb50a774e8e0b0d
ansh3108 <anshkumar9686@gmail.com>
2026-04-25 18:05:11 +0530

    init

---
+++ src/main/java/com/dimension/fourthdimension/FourthDimensionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dimension.fourthdimension;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FourthDimensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
